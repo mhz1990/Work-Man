762e9732e77675961907402858bb4988289a6c04
Matt Hoadley <matt.hoadley1990@gmail.com>
2023-04-25 04:31:31 +0800

    if this commit is applied, it would complete Feature 03

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     description = models.TextField()
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
